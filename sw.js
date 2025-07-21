890fb74845e2938719138ca5b279bd7e222c1084
silassequeira <silassequeiraa@gmail.com>
2025-07-21 20:33:28 +0100

    Update service worker registration path and cache additional assets

--- sw.js
+++ sw.js
@@ -7,6 +7,10 @@ const urlsToCache = [
     '/css/minified/components.css',
     '/images/sprites.svg',
     '/images/coimbra-sports-events-illustration.png',
+    '/images/favicon/site.webmanifest',
+    '/images/favicon/favicon.ico',
+    '/images/favicon/android-chrome-192x192.png',
+    '/images/favicon/android-chrome-512x512.png',
     '/images/uploads/cycling-event-3.webp',
     '/images/uploads/multi-sport-event-6.webp',
     '/images/uploads/running-event-1.webp',
@@ -14,7 +18,19 @@ const urlsToCache = [
     '/images/uploads/volleyball-event-4.webp',
     '/images/uploads/yoga-event-5.webp',
     '/js/svg-loader.js',
-    '/js/script.js'
+    '/js/script.js',
+    '/js/app/app.js',
+    '/js/app/date-picker.js',
+    '/js/app/event-display.js',
+    '/js/app/event-form.js',
+    '/js/app/event-modal.js',
+    '/js/app/event-registration.js',
+    '/js/app/event-slider.js',
+    '/js/app/events-data.js',
+    '/js/app/modal-manager.js',
+    '/js/app/notifications.js',
+    '/js/app/ui.js',
+    '/js/app/utils.js'
 ];
 
 self.addEventListener('install', event => {
